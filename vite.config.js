d1708d117ffd02dfa2670f8148f9eaa997a48343
Falu Zaraj SM <faluzaraj@gmail.com>
2026-06-03 09:19:19 -0500

    Actualizacion y eliminacion del Malware detectado V2

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "node:url";
 import { dirname, resolve } from "node:path";
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
